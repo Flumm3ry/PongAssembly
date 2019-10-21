d20db11fff30bbc6b5c721caafd50dc3f40a7119
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-10-21 19:38:45 +1100

    Moved the draw player function to a different file

--- kernel7.asm
+++ kernel7.asm
@@ -70,30 +70,8 @@ b MainLoop  ;loop forever
 CoreLoop: ; Infinite Loop For Core 1..3
   b CoreLoop
 
-DrawPlayer:
-; 4 PARAMS
-; Screen address, x, y, colour
-     mov r4,r1
-     mov r5,r1
-     add r5,#5
-     mov r6,r2
-     add r6,#40
-     rectangleloop:
-        mov r1,r4
-        lineloop:
-              push {lr}
-              push {r0-r9}
-              bl drawpixel
-              pop {r0-r9}
-              pop {lr}
-              add r1,#1
-              cmp r1,r5
-        blo lineloop
-        add r2,#1
-        cmp r2,r6
-     blo rectangleloop
-bx lr
 
+include "drawplayer_2param.asm"
 include "FBinit8.asm"
 include "timer2_2Param.asm"
 include "drawpixel.asm"
